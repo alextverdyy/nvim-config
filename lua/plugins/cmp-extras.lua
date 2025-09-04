30732d2d7d23b27874b02d3b6be1400538cacf22
Oleg Tverdyy Tereshchenko <alextverdyy@gmail.com>
2025-09-04 17:43:44 -0400

    feat(wip): Update config

---
+++ lua/plugins/cmp-extras.lua
@@ -0,0 +1,8 @@
+return {
+  "hrsh7th/nvim-cmp",
+  dependencies = { "chrisgrieser/cmp-nerdfont" },
+  ---@param opts cmp.ConfigSchema
+  opts = function(_, opts)
+    table.insert(opts.sources, { name = "nerdfont" })
+  end,
+}

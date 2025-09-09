703d05db070893d63d73b32642c1e53added4acd
Oleg Tverdyy Tereshchenko <alextverdyy@gmail.com>
2025-09-09 16:02:44 -0400

    feat(wip): Update config lsp

--- lua/plugins/cmp-extras.lua
+++ lua/plugins/cmp-extras.lua
@@ -1,8 +1,22 @@
 return {
-  "hrsh7th/nvim-cmp",
-  dependencies = { "chrisgrieser/cmp-nerdfont" },
-  ---@param opts cmp.ConfigSchema
-  opts = function(_, opts)
-    table.insert(opts.sources, { name = "nerdfont" })
-  end,
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = { "chrisgrieser/cmp-nerdfont" },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      table.insert(opts.sources, { name = "nerdfont" })
+    end,
+  },
+  {
+    "saghen/blink.cmp",
+    dependencies = { "hrsh7th/nvim-cmp" },
+    config = function()
+      require("blink-cmp").setup({
+        sources = {
+          { name = "blink_cmp" },
+          { name = "nvim_lsp" },
+        },
+      })
+    end,
+  },
 }

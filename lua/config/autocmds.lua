2c960a7bdaceae48d3ecebe484185395168f25e0
Oleg Tverdyy Tereshchenko <alextverdyy@gmail.com>
2025-10-06 09:44:22 +0200

    config

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,1 +1,11 @@
 -- Add needed auto cmd
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  pattern = { "tmux.conf", ".tmux.conf" },
+  callback = function()
+    vim.lsp.enable("tmux")
+    vim.lsp.start({
+      name = "tmux",
+      cmd = { "tmux-language-server" },
+    })
+  end,
+})
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,14 +1,16 @@
 return {
   "neovim/nvim-lspconfig",
-  opts = {
-    inlay_hints = { enabled = false },
-    servers = {
+  opts = function(_, opts)
+    local keys = require("lazyvim.plugins.lsp.keymaps").get()
+    keys[#keys + 1] = { "K", require("hover").open }
+    opts.inlay_hints = { enabled = false }
+    opts.servers = {
       lua_ls = {},
       sourcekit = {},
       bashls = {},
       rust_analyzer = {},
       basedpyright = {},
       fish_lsp = {},
-    },
-  },
+    }
+  end,
 }
